38baa400be9c9f4d45cd10aa43c2261610ebe16f
Yogeswaran <yogesvetri2007@gmail.com>
2026-03-25 18:59:19 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,43 @@
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    if b == 0:
+        return "Cannot divide by zero"
+    return a / b
+
+
+print("Simple Calculator")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
+
+choice = int(input("Enter choice: "))
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+if choice == 1:
+    print("Result:", add(num1, num2))
+
+elif choice == 2:
+    print("Result:", subtract(num1, num2))
+
+elif choice == 3:
+    print("Result:", multiply(num1, num2))
+
+elif choice == 4:
+    print("Result:", divide(num1, num2))
+
+else:
+    print("Invalid choice")
